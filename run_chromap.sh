4a10d9ada7c697a8bad700ae96b147fbe376ac5a
keen-laras <kinanti.seraphina@gmail.com>
2025-12-09 03:40:30 +0800

    Create run_chromap.sh

---
+++ run_chromap.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# 输入路径
+CONTIGS="/path/to/{input}.fa"
+HIC_DIR="/path/to/HicFiles"
+THREADS=32
+
+# 工具路径
+SAMTOOLS="/share/app/samtools/1.11/bin/samtools"
+
+# 输出前缀
+OUT_PREFIX="{output}"
+
+echo "Indexing contig assembly"
+$SAMTOOLS faidx $CONTIGS
+/opt/software/chromap_env/bin/chromap -i -r $CONTIGS -o ${OUT_PREFIX}.index
+
+# 三对 Hi-C 数据编号
+#SAMPLES=("687" "688" "689")
+
+# 循环处理三对
+#for ID in "${SAMPLES[@]}"; do
+#    echo "=== Step 2: 处理Hi-C数据对 ${ID} ==="
+    R1="${HIC_DIR}/{HiC}_R1.fastq"
+    R2="${HIC_DIR}/{HiC}_R2.fastq"
+    
+    OUT="${OUT_PREFIX}}"
+
+    echo "  -> Reading ${R1} and ${R2}"
+    /opt/software/chromap_env/bin/chromap \
+      --preset hic \
+      -r $CONTIGS \
+      -x ${OUT_PREFIX}.index \
+      --remove-pcr-duplicates \
+      -1 $R1 \
+      -2 $R2 \
+      --SAM \
+      -o ${OUT}.sam \
+      -t $THREADS
+
+    echo "  -> Converting SAM to BAM"
+    $SAMTOOLS view -bh ${OUT}.sam | \
+      $SAMTOOLS sort -@ $THREADS -n -o ${OUT}.bam
+
+    rm ${OUT}.sam
+    echo "=== Output written to ${OUT}.bam ==="
+done
+
+echo "=== Processing Completed ==="
